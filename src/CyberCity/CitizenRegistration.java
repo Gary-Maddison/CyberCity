bed90f37853b9485b2c7da4d33cdd1e39091d864
Gary-Maddison <garyfmaddison@gmail.com>
2026-02-11 17:36:12 +0000

    Citizen Registration Updated

--- src/CyberCity/CitizenRegistration.java
+++ src/CyberCity/CitizenRegistration.java
@@ -1,11 +1,49 @@
 package CyberCity;
 
+import java.util.Scanner;
+
 public class CitizenRegistration {
 
     void main() {
 
-        String name = "Test";
-        System.out.println(name);
+        System.out.println("Welcome to Cyber City\n");
+        Scanner userInput = new Scanner(System.in);
+        int numberOfCitizens = (0);
 
+        while (true) {
+            System.out.println("Please enter your name: ");
+            String citizenName = userInput.nextLine();
+
+            if (citizenName.equalsIgnoreCase("exit")) {
+                break;
+            }
+
+            System.out.println("Please enter your job title: ");
+            String jobTitle = userInput.nextLine();
+
+            System.out.println("Please enter your monthly credits: ");
+            double monthlyCredits = userInput.nextDouble();
+            userInput.nextLine();
+
+            if (jobTitle.equals("Hacker") || jobTitle.equals("Android")) {
+                System.out.println("Welcome to the underground " + citizenName);
+            }
+            else if (monthlyCredits > 10_000 && !jobTitle.equals("Politician")) {
+                System.out.println("Welcome, High-Value Citizen");
+            }
+            else {
+                System.out.println("Welcome to the Slums");
+            }
+
+            double totalTax = monthlyCredits * 0.12D;
+            System.out.println("Your Contribution to the City:" + totalTax + " credits.");
+
+            String isThreat = (jobTitle.equals("Hacker")) ? "Red" : "Green";
+            System.out.println("Security Clearance: " + isThreat);
+
+            numberOfCitizens++;
+        }
+        System.out.println(" ");
+        System.out.println(numberOfCitizens + " citizens registered");
     }
 }
